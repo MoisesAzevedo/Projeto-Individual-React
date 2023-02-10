c5f9dc01f0610a81113641ddcedb36db880fe075
MoisesAzevedo <moisesdev2022@gmail.com>
2023-02-09 21:14:14 -0300

    corrigindo o link

--- src/componentes/Rotas.jsx
+++ src/componentes/Rotas.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import '../App.css';
+import "../App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from './Header';
-import Home from './paginas/Home'
+import Header from "./Header";
+import Home from "./paginas/Home";
 import Users from "./paginas/Users.jsx";
 
-function Rotas(){
-    return (
-      <Router>
-        <Header />
-        <div className="container">
-          <Routes>
-            <Route path="/" exact element={<Home />}/>
-            <Route path="/users" element={<Users />} />
-          </Routes>
-        </div>
-      </Router>
-    
-    );
-     
+function Rotas() {
+  return (
+    <Router>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/Projeto-Individual-React" exact element={<Home />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default Rotas;
